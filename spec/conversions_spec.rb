3eb210d0274590b227f3f30ecc69c8fff251ae67
Tina Hang <mstinahang@gmail.com>
2019-12-24 03:25:58 +0000

    Done.

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -6,5 +6,10 @@ describe "conversions" do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
+
+    it 'given 100, returns 2834.95' do
+      grams = ounces_to_grams(100)
+      expect(grams).to eq(2834.95)
+    end
   end
 end
